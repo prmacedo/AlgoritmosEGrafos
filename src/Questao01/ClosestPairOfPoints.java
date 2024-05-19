d201c1c870912ae635f5f12626a8c46fd4571dd7
Paulo Macêdo <p.ricardomacedo@hotmail.com>
2024-05-19 09:01:17 -0300

    implementação das sooluções por força bruta e por divisão e conquista da questão 1

--- src/Questao01/ClosestPairOfPoints.java
+++ src/Questao01/ClosestPairOfPoints.java
@@ -1,35 +1,216 @@
 package Questao01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ClosestPairOfPoints {
     private static double distanceBetweenPoints(Point p1, Point p2) {
-        double squareDiffX = Math.pow(p1.x() - p2.x(), 2);
-        double squareDiffY = Math.pow(p1.y() - p2.y(), 2);
-        return Math.sqrt(squareDiffX + squareDiffY);
+        return Math.sqrt(Math.pow(p1.x() - p2.x(), 2) + Math.pow(p1.y() - p2.y(), 2));
     }
 
-    public static Point[] solutionBruteForce(List<Point> points) {
-        Point[] pointsPair = null;
+    public static Point[] bruteForceSolution(Point[] points) {
         double minDistance = Double.MAX_VALUE;
+        Point[] closestPair = new Point[2];
 
-        for (int i = 1; i < points.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                double distance = distanceBetweenPoints(points.get(i), points.get(j));
+        for (int i = 0; i < points.length - 1; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                double distance = distanceBetweenPoints(points[i], points[j]);
                 if (distance < minDistance) {
                     minDistance = distance;
-                    pointsPair = new Point[] {points.get(i), points.get(j)};
+                    closestPair[0] = points[i];
+                    closestPair[1] = points[j];
                 }
             }
         }
 
-        return pointsPair;
+        return closestPair;
     }
 
-    public static Point[] solutionDivideAndConquer(List<Point> points) {
-        Point[] pointsPair = null;
-        double minDistance = Double.MAX_VALUE;
+    public static Point[] divideAndConquerSolution(Point[] points) {
+        Point[] pointsByX = points.clone();
+        mergeSortByXCoordinate(pointsByX, 0, points.length - 1);
 
-        return pointsPair;
+        Point[] pointsByY = points.clone();
+        mergeSortByYCoordinate(pointsByY, 0, points.length - 1);
+
+        return closestPairOfPoints(pointsByX, pointsByY);
+    }
+
+    private static Point[] closestPairOfPoints(Point[] pointsX, Point[] pointsY) {
+        int size = pointsX.length;
+
+        if (size <= 3) {
+            return bruteForceSolution(pointsX);
+        }
+
+        int middle = size / 2;
+
+        Point[] pointsXLeft = Arrays.copyOfRange(pointsX, 0, middle);
+        Point[] pointsXRight = Arrays.copyOfRange(pointsX, middle, size);
+
+        Point middlePoint = pointsX[middle];
+
+        List<Point> pointsYLeftList = new ArrayList<>();
+        List<Point> pointsYRightList = new ArrayList<>();
+
+        for (Point point : pointsY) {
+            if (point.x() <= middlePoint.x()) {
+                pointsYLeftList.add(point);
+            } else {
+                pointsYRightList.add(point);
+            }
+        }
+
+        Point[] pointsYLeft = pointsYLeftList.toArray(new Point[0]);
+        Point[] pointsYRight = pointsYRightList.toArray(new Point[0]);
+
+        Point[] closestPairLeft = closestPairOfPoints(pointsXLeft, pointsYLeft);
+        Point[] closestPairRight = closestPairOfPoints(pointsXRight, pointsYRight);
+
+        double distanceLeft = distanceBetweenPoints(closestPairLeft[0], closestPairLeft[1]);
+        double distanceRight = distanceBetweenPoints(closestPairRight[0], closestPairRight[1]);
+        double distance = Math.min(distanceLeft, distanceRight);
+
+        Point[] bestPair = distanceLeft < distanceRight ? closestPairLeft : closestPairRight;
+
+        List<Point> strip = new ArrayList<>();
+        for (Point point : pointsY) {
+            if (Math.abs(point.x() - middlePoint.x()) < distance) {
+                strip.add(point);
+            }
+        }
+
+        Point[] stripArray = strip.toArray(new Point[0]);
+
+        Point[] stripPair = stripClosest(stripArray, distance);
+        if (stripPair != null && distanceBetweenPoints(stripPair[0], stripPair[1]) < distance) {
+            return stripPair;
+        }
+
+        return bestPair;
+    }
+
+    private static Point[] stripClosest(Point[] points, double distance) {
+        double min = distance;
+        Point[] pairOfPoints = null;
+
+        for (int i = 0; i < points.length - 1; ++i) {
+            for (int j = i + 1; j < points.length && (points[j].y() - points[i].y()) < min; ++j) {
+                double currentDistance = distanceBetweenPoints(points[i], points[j]);
+                if (currentDistance < min) {
+                    min = currentDistance;
+                    pairOfPoints = new Point[] {points[i], points[j]};
+                }
+            }
+        }
+
+        return pairOfPoints;
+    }
+
+    private static void mergeSortByXCoordinate(Point[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            mergeSortByXCoordinate(arr, l, m);
+            mergeSortByXCoordinate(arr, m + 1, r);
+
+            mergeByXCoordinate(arr, l, m, r);
+        }
+    }
+
+    private static void mergeByXCoordinate(Point[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Point[] L = new Point[n1];
+        Point[] R = new Point[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[l + i];
+        }
+
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].x() <= R[j].x()) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void mergeSortByYCoordinate(Point[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            mergeSortByYCoordinate(arr, l, m);
+            mergeSortByYCoordinate(arr, m + 1, r);
+
+            mergeByYCoordinate(arr, l, m, r);
+        }
+    }
+
+    private static void mergeByYCoordinate(Point[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Point[] L = new Point[n1];
+        Point[] R = new Point[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[l + i];
+        }
+
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].y() <= R[j].y()) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 }
--- src/Questao01/Test.java
+++ src/Questao01/Test.java
@@ -1,57 +1,68 @@
 package Questao01;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        List<Point> points10 = generatePoints(10);
-        List<Point> points100 = generatePoints(100);
-        List<Point> points250 = generatePoints(250);
-        List<Point> points500 = generatePoints(500);
-        List<Point> points1000 = generatePoints(1000);
-        List<Point> points2000 = generatePoints(2000);
+        Point[] points10 = generatePoints(100000);
+//        Point[] points100 = generatePoints(100);
+//        Point[] points250 = generatePoints(250);
+//        Point[] points500 = generatePoints(500);
+//        Point[] points1000 = generatePoints(1000);
+//        Point[] points2000 = generatePoints(2000);
 
-        System.out.println(Arrays.toString(points10.toArray()));
+        System.out.println(Arrays.toString(points10));
 
-        // Brute Force - Warm Up
+//        System.out.println(Arrays.toString(points10.toArray()));
+//
+//        // Brute Force - Warm Up
         analyzeBruteForce(points10);
-
-        // 100 Points
-        analyzeBruteForce(points100);
-
-        // 250 Points
-        analyzeBruteForce(points250);
-
-        // 500 Points
-        analyzeBruteForce(points500);
-
-        // 1000 Points
-        analyzeBruteForce(points1000);
-
-        // 2000 Points
-        analyzeBruteForce(points2000);
+        analyzeDivideAndConquer(points10);
+//
+//        // 100 Points
+//        analyzeBruteForce(points100);
+//
+//        // 250 Points
+//        analyzeBruteForce(points250);
+//
+//        // 500 Points
+//        analyzeBruteForce(points500);
+//
+//        // 1000 Points
+//        analyzeBruteForce(points1000);
+//
+//        // 2000 Points
+//        analyzeBruteForce(points2000);
     }
 
-    private static void analyzeBruteForce(List<Point> points) {
+    private static void analyzeBruteForce(Point[] points) {
         long start;
         long end;
 
         start = System.currentTimeMillis();
-        ClosestPairOfPoints.solutionBruteForce(points);
+        System.out.println(Arrays.toString(ClosestPairOfPoints.bruteForceSolution(points)));
         end = System.currentTimeMillis();
-        System.out.println(points.size() + " points: " + (end - start) + "ms");
+        System.out.println(points.length + " points: " + (end - start) + "ms");
     }
 
-    public static List<Point> generatePoints(int size) {
-        List<Point> points = new ArrayList<>();
+    private static void analyzeDivideAndConquer(Point[] points) {
+        long start;
+        long end;
+
+        start = System.currentTimeMillis();
+        System.out.println(Arrays.toString(ClosestPairOfPoints.divideAndConquerSolution(points)));
+        end = System.currentTimeMillis();
+        System.out.println(points.length + " points: " + (end - start) + "ms");
+    }
+
+    public static Point[] generatePoints(int size) {
+        Point[] points = new Point[size];
         Random random = new Random(42);
         for (int i = 0; i < size; i++) {
-            int x = random.nextInt(100);
-            int y = random.nextInt(100);
-            points.add(new Point(x, y));
+            int x = random.nextInt(2 * size - 1) - size;
+            int y = random.nextInt(2 * size - 1) - size;
+            points[i] = new Point(x, y);
         }
         return points;
     }
