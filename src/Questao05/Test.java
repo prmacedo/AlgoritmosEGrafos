77c3215b8cb03f2722f53a297bd3096886f13ff3
Paulo Macêdo <p.ricardomacedo@hotmail.com>
2024-05-19 12:03:31 -0300

    analizar tempo de execução para diferentes entradas

--- src/Questao05/Test.java
+++ src/Questao05/Test.java
@@ -4,10 +4,40 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        int[] votes = generateArray(10, 43);
+        int[] votes1 = generateArray(10, 43);
+        int[] votes2 = generateArray(100, 43);
+        int[] votes3 = generateArray(1_000, 43);
+        int[] votes4 = generateArray(10_000, 43);
+        int[] votes5 = generateArray(100_000, 43);
 
-        analyzeBruteForce(votes);
-        analyzeDivideAndConquer(votes);
+        // Warm-up
+        analyzeBruteForce(votes1);
+        System.out.println();
+
+        // 10 Votes
+        analyzeBruteForce(votes1);
+        analyzeDivideAndConquer(votes1);
+        System.out.println();
+
+        // 100 Votes
+        analyzeBruteForce(votes2);
+        analyzeDivideAndConquer(votes2);
+        System.out.println();
+
+        // 1_000 Votes
+        analyzeBruteForce(votes3);
+        analyzeDivideAndConquer(votes3);
+        System.out.println();
+
+        // 10_000 Votes
+        analyzeBruteForce(votes4);
+        analyzeDivideAndConquer(votes4);
+        System.out.println();
+
+        // 100_000 Votes
+        analyzeBruteForce(votes5);
+        analyzeDivideAndConquer(votes5);
+        System.out.println();
     }
 
     public static int[] generateArray(int size, int seed) {
@@ -25,19 +55,19 @@ public class Test {
         long start;
         long end;
 
-        start = System.currentTimeMillis();
+        start = System.nanoTime();
         System.out.println(MajorityVoting.bruteForceSolution(votes));
-        end = System.currentTimeMillis();
-        System.out.println("Time: " + (end - start) + "ms");
+        end = System.nanoTime();
+        System.out.println("Time: " + (end - start) / 1_000_000d + "ms");
     }
 
     private static void analyzeDivideAndConquer(int[] votes) {
         long start;
         long end;
 
-        start = System.currentTimeMillis();
+        start = System.nanoTime();
         System.out.println(MajorityVoting.divideAndConquerSolution(votes));
-        end = System.currentTimeMillis();
-        System.out.println("Time: " + (end - start) + "ms");
+        end = System.nanoTime();
+        System.out.println("Time: " + (end - start) / 1_000_000d + "ms");
     }
 }
